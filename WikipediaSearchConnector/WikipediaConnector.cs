8b42014389a7a6a9d040bb4ba86b0d8ac598254b
Brandon Nesiba <Brandon.Nesiba@veteransunited.com>
2024-03-21 23:35:05 -0500

    wip: skeleton of wikipediaconnector

--- WikipediaSearchConnector/WikipediaConnector.cs
+++ WikipediaSearchConnector/WikipediaConnector.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WikipediaSearchConnector.Models;
 
 namespace WikipediaSearchConnector
 {
     internal class WikipediaConnector
     {
+        private readonly HttpClient _httpClient;
+        //TODO: get from config?
+        private Uri baseUrl = new Uri("https://en.wikipedia.org/w/api.php?format=json&action=query");
+
+        public WikipediaConnector(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
     }
 }
